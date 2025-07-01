c9417a926e6befb1d5d8e8748f32bafed5535890
shawonmondol-shibu <shawonmondolshibu@gmail.com>
2025-07-01 18:21:20 +0600

    skeleton  updated

--- components/shared/skeleton.tsx
+++ components/shared/skeleton.tsx
@@ -7,7 +7,7 @@ export default function Skeleton() {
         <div className="bg-muted/50 aspect-video rounded-xl" />
         <div className="bg-muted/50 aspect-video rounded-xl" />
         <div className="bg-muted/50 aspect-video rounded-xl" />
-      </div>
+      </div> 
       <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
     </>
   );
