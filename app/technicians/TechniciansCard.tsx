9e8117443b28e6f50153595364a9452eed5efd11
shawonmondol-shibu <shawonmondolshibu@gmail.com>
2025-07-01 22:07:17 +0600

    technicians updated

--- app/technicians/TechniciansCard.tsx
+++ app/technicians/TechniciansCard.tsx
@@ -1,48 +1,54 @@
 import { Button } from "@/components/ui/button";
 import {
   Card,
-  CardAction,
   CardContent,
-  CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
 import { UserCircle2 } from "lucide-react";
-import Image from "next/image";
+// import Image from "next/image";
 import React from "react";
 
 export default function TechniciansCard() {
   return (
-    <div>
-      <Card className="w-full max-w-max rounded-4xl">
+    <>
+      <Card className="w-full max-w-max rounded-4xl px-6 py-10 m-auto">
         <CardHeader>
-            <span className="rounded-full border-5 border-orange-400 w-fit p-4 ">
-
-            <UserCircle2 size={50}/>
-            </span>
-            {/* <Image src={''} width={230} height={230} alt=""/> */}
+          <span className="rounded-full border-5 border-orange-400 w-[90px] h-[90px] flex ">
+            <UserCircle2 size={50} className="m-auto"/>
+          </span>
+          {/* <Image src={''} width={230} height={230} alt=""/> */}
         </CardHeader>
         <CardContent>
-
-          <p className="text-base text-orange-400 font-normal">Automation Engineer</p>
+          <p className="text-base text-orange-400 font-normal">
+            Automation Engineer
+          </p>
           <CardTitle className="text-2xl">A.K Jayawardhana</CardTitle>
-<div>
-    <p>ID :</p>
-    <p>Contact :</p>
-</div>
-
+          <div className="grid grid-cols-2 items-center mt-4">
+            <p>ID :</p>
+            <p>73456</p>
+            <p>Contact :</p>
+            <p>077 34 76 300</p>
+          </div>
         </CardContent>
-        <CardFooter>
-          <CardAction>
-            <div  className="flex gap-10 items-center justify-between">
- 
-            <Button variant={'default'} size={"default"} className="bg-orange-400">Edit</Button>
-            <Button variant={'default'} size={"default"} className="bg-(--darkOrange)">Disable</Button>
-            </div>
-          </CardAction>
+        <CardFooter className="flex items-center justify-between">
+          <Button
+            variant={"default"}
+            size={"default"}
+            className="bg-orange-400"
+          >
+            Edit
+          </Button>
+          <Button
+            variant={"default"}
+            size={"default"}
+            className="bg-(--darkOrange)"
+          >
+            Disable
+          </Button>
         </CardFooter>
       </Card>
-    </div>
+    </>
   );
 }
--- app/technicians/page.tsx
+++ app/technicians/page.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
-import TechniciansCard from './TechniciansCard'
+import React, { Suspense } from "react";
+import TechniciansCard from "./TechniciansCard";
+import Skeleton from "@/components/shared/skeleton";
 
 export default function Page() {
   return (
     <div>
+      <main>
+        <Suspense fallback={<Skeleton />}>
+        <div className="grid grid-cols-3 gap-5 items-center justify-center">
 
-        <TechniciansCard/>
+          {Array.from({ length: 9 }).map((item, i: number) => (
+              <TechniciansCard key={i} />
+            ))}
+            </div>
+        </Suspense>
+      </main>
     </div>
-  )
+  );
 }
