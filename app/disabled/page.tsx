28a319d1b8edef2184726a9acb4d7b66f4c1133d
shawonmondol-shibu <shawonmondolshibu@gmail.com>
2025-07-02 07:48:50 +0600

    Disabled updated

--- app/disabled/page.tsx
+++ app/disabled/page.tsx
@@ -24,7 +24,7 @@ export default function Page() {
       </div>
       <main className="mt-8">
         <Suspense fallback={<Skeleton />}>
-          <div className="grid grid-cols-3 gap-5 items-center justify-center">
+          <div className="xl:grid grid-cols-3 flex flex-wrap gap-5 items-center justify-center">
             {Array.from({ length: 9 }).map((item, i: number) => (
               <TechniciansCard className="bg-[#f9000025] shadow-none" key={i} />
             ))}
